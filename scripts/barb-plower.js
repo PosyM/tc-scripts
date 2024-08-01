ff89eb4fc4502235f8e470202fca73428ff9eb1c
Matus1k <matus.posypanka3@gmail.com>
2024-08-01 16:43:26 +0200

    feat(app): remove scrapper and replace it with actual script

---
+++ scripts/barb-plower.js
@@ -0,0 +1,6 @@
+/*
+    author: Posy
+    description: Script for cating non-pits buildings, therefore maximizing neutral villages' production. Script is meant to be run from report page.
+ */
+
+console.log('barb plower')
